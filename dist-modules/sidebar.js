7bef83689c5bf8d700564453100b209206e00b07
sugarshin <shinsugar@gmail.com>
2016-05-29 00:50:13 +0900

    log

--- dist-modules/sidebar.js
+++ dist-modules/sidebar.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
@@ -17,8 +19,6 @@ var _reactDom = require('react-dom');
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -76,7 +76,7 @@ var defaultStyles = {
   }
 };
 
-var Sidebar = (function (_React$Component) {
+var Sidebar = function (_React$Component) {
   _inherits(Sidebar, _React$Component);
 
   function Sidebar(props) {
@@ -198,7 +198,7 @@ var Sidebar = (function (_React$Component) {
   }, {
     key: 'inCancelDistanceOnScroll',
     value: function inCancelDistanceOnScroll() {
-      var cancelDistanceOnScroll = undefined;
+      var cancelDistanceOnScroll = void 0;
 
       if (this.props.pullRight) {
         cancelDistanceOnScroll = Math.abs(this.state.touchCurrentX - this.state.touchStartX) < CANCEL_DISTANCE_ON_SCROLL;
@@ -264,9 +264,10 @@ var Sidebar = (function (_React$Component) {
       var useTouch = this.state.dragSupported && this.props.touch;
       var isTouching = this.isTouching();
       var rootProps = {
+        className: this.props.rootClassName,
         style: _extends({}, defaultStyles.root, this.props.styles.root)
       };
-      var dragHandle = undefined;
+      var dragHandle = void 0;
 
       // sidebarStyle right/left
       if (this.props.pullRight) {
@@ -375,7 +376,7 @@ var Sidebar = (function (_React$Component) {
   }]);
 
   return Sidebar;
-})(_react2.default.Component);
+}(_react2.default.Component);
 
 Sidebar.propTypes = {
   // main content to render
